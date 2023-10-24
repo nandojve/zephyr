59b1fae821aec19d00f59ce8c85ef604f0d7360d
Gerson Fernando Budke <gerson.budke@leica-geosystems.com>
2023-10-24 15:58:41 +0200

    renode: Test ADCs
    Signed-off-by: Gerson Fernando Budke <gerson.budke@leica-geosystems.com>

--- boards/arm/nucleo_f767zi/board.cmake
+++ boards/arm/nucleo_f767zi/board.cmake
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
+set(SUPPORTED_EMU_PLATFORMS renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/nucleo_f767zi.resc)
+set(RENODE_UART sysbus.usart3)
+
 board_runner_args(jlink "--device=STM32F767ZI" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
--- boards/arm/nucleo_l073rz/board.cmake
+++ boards/arm/nucleo_l073rz/board.cmake
@@ -1,8 +1,11 @@
 # SPDX-License-Identifier: Apache-2.0
 
+set(SUPPORTED_EMU_PLATFORMS renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/nucleo_l073rz.resc)
+set(RENODE_UART sysbus.usart2)
+
 board_runner_args(jlink "--device=STM32L073RZ" "--speed=4000")
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
-
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)

357ead13f901028051388f3d32433e73fe4de2b8
Gerson Fernando Budke <gerson.budke@ossystems.com.br>
2023-12-29 14:37:57 +0100

    nucleo_g071rb: Add renode simulation to test ADC
    Signed-off-by: Gerson Fernando Budke <gerson.budke@ossystems.com.br>

--- boards/arm/nucleo_g071rb/board.cmake
+++ boards/arm/nucleo_g071rb/board.cmake
@@ -1,4 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
+
+set(SUPPORTED_EMU_PLATFORMS renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/nucleo_g071rb.resc)
+set(RENODE_UART sysbus.usart2)
+
 board_runner_args(pyocd "--target=stm32g071rbtx")
 board_runner_args(pyocd "--flash-opt=-O reset_type=hw")
 board_runner_args(pyocd "--flash-opt=-O connect_mode=under-reset")

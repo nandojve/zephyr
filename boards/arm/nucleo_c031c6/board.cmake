100d404b8a42180ee8a2ba7622e1b475da19acc8
Gerson Fernando Budke <gerson.budke@ossystems.com.br>
2023-12-29 15:15:10 +0100

    nucleo_c031c6: Add renode simulation to test ADC
    Signed-off-by: Gerson Fernando Budke <gerson.budke@ossystems.com.br>

--- boards/arm/nucleo_c031c6/board.cmake
+++ boards/arm/nucleo_c031c6/board.cmake
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
+set(SUPPORTED_EMU_PLATFORMS renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/nucleo_c031c6.resc)
+set(RENODE_UART sysbus.usart2)
+
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
 
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)

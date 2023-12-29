c0d690b00cc3c308366bde201efff4fa901ccb79
Gerson Fernando Budke <gerson.budke@ossystems.com.br>
2023-12-29 15:06:09 +0100

    b_l072z_lrwan1: Add renode simulation to test ADC
    Signed-off-by: Gerson Fernando Budke <gerson.budke@ossystems.com.br>

--- boards/arm/b_l072z_lrwan1/board.cmake
+++ boards/arm/b_l072z_lrwan1/board.cmake
@@ -1,5 +1,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
+set(SUPPORTED_EMU_PLATFORMS renode)
+set(RENODE_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/support/b_l072z_lrwan1.resc)
+set(RENODE_UART sysbus.usart2)
+
 board_runner_args(jlink "--device=STM32L072CZ" "--speed=4000")
 
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
